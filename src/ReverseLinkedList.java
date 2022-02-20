e3c66934b83ee0ed63a6323bf0cd48504a8e222c
shilpa <shilpa511raghav@gmail.com>
2022-02-20 07:40:18 +0530

    reverse a linked list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,10 +1,23 @@
 public class ReverseLinkedList {
+    static Node head;
 
     public static void main(String[] args) {
+        head = new Node(3,new Node(5,new Node(7,null)));
 
+        Node newHead = reverseLinkList(head);
+        printLL(newHead);
     }
 
-    class Node{
+    static void printLL(Node head){
+        Node cur = head;
+        while(cur.next != null){
+            System.out.println(cur.data);
+            cur = cur.next;
+        }
+        System.out.println(cur.data);
+    }
+
+    static class Node{
         int data;
         Node next;
 
