2b1cde4483ef10f52d642eee049365f7f293b5b0
shilpa <shilpa511raghav@gmail.com>
2022-02-20 05:52:48 +0530

    revrse a link list

--- src/ReverseLinkedList.java
+++ src/ReverseLinkedList.java
@@ -1,3 +1,26 @@
 public class ReverseLinkedList {
 
+    public static void main(String[] args) {
+
+    }
+
+    class Node{
+        int data;
+        Node next;
+
+        Node(int data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public static Node reverseLinkList(Node head){
+        Node prev = null;
+        Node temp = null;
+        Node cur = head;
+        while(cur != null){
+
+        }
+    }
+
 }
