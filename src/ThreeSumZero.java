dffe3001a5e41f0cd48356b63b98d62c0e255ce2
shilpa <shilpa511raghav@gmail.com>
2022-12-14 09:13:08 +0530

    sum of three numbers is zero

--- src/ThreeSumZero.java
+++ src/ThreeSumZero.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class ThreeSumZero {
     public static void main(String[] args) {
-        System.out.println(threeSum(new int []{-1,0,1,2,-1,-4}));
+        System.out.println(threeSumO(new int []{-1,0,1,2,-1,-4}));
         System.out.println(threeSumOptimal(new int[]{-1,0,1,2,-1,-4}));
     }
 
@@ -38,6 +38,27 @@ public class ThreeSumZero {
                     j++;
                     k--;
                 }else if(sum >0){
+                    k--;
+                }else{
+                    j++;
+                }
+            }
+        }
+        return new ArrayList(set);
+    }
+
+    public static List<List<Integer>> threeSumO(int[] ar){
+        if(ar.length<3) return new ArrayList();
+        Arrays.sort(ar);
+        HashSet<List<Integer>> set = new HashSet<>();
+        for(int i=0;i<ar.length-2;i++){
+            int j = i+1;
+            int k = ar.length-1;
+            while(j<k){
+                int sum = ar[i]+ar[j]+ar[k];
+                if(sum== 0){
+                    set.add(Arrays.asList(ar[i],ar[j++],ar[k--]));
+                }else if(sum>0){
                     k--;
                 }else{
                     j++;
