456961fbed3585001515ca6503dc97483bfe8c4a
shilpa <shilpa511raghav@gmail.com>
2022-12-25 16:49:01 +0530

    sort and compare elements and add in new response list of int array and convert it to integer array while sending response

---
+++ src/MediumLevel/MergeIntervals.java
@@ -0,0 +1,38 @@
+package MediumLevel;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MergeIntervals {
+    public static void main(String[] args) {
+        int[][] output = mergeIntervals(new int[][]{{22, 28}, {1, 8}, {25, 27}, {17, 19}, {27, 30}, {5, 12}});
+        System.out.println(Arrays.deepToString(output));
+    }
+
+    // create empty list of integer array as response array
+    //first sort the 2D array
+    // then take first array from intervals in separate array and then loop from 2nd array i.e i=1 and compare separate arrays second element with first element of i array in loop
+    //if separate array second element < first element of i array in loop  then add this separate array to response array and replace separate array with i array in loop and so on
+    //else find max from second element of separate array and second element from i array and replace this max value to the second element of seperate array and so on
+    //edge case if loop completed and element in separate array has been present and not being added  then add that element also in separate array
+    public static int[][] mergeIntervals(int[][] intervals) {
+
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        List<int[]> res = new ArrayList();
+        if (intervals.length <= 1) {
+            return intervals;
+        }
+        int[] in = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            if (in[1] < intervals[i][0]) {
+                res.add(in);
+                in = intervals[i];
+            } else {
+                in[1] = intervals[i][1] > in[1] ? intervals[i][1] : in[1]; // can use Math.max as well to find max between both
+            }
+        }
+        res.add(in);
+        return res.toArray(new int[res.size()][]);
+    }
+}
---
+++ src/SqauresOfSortedArray.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class SqauresOfSortedArray {
+    public static void main(String[] args) {
+        squaresOfSortedArray(new int[]{-4,-1,0,3,10});
+    }
+
+
+    //In place and  time complexity = nlogn as arrays.sort take nlogn
+    //Space complexity = O(N) as no new space used
+    public int[] sortedSquares(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            nums[i] = nums[i] * nums[i];
+        }
+        Arrays.sort(nums);
+        return nums;
+    }
+
+    //take two pointer left and right and compare both square and then shift that number whoz square is larger to the end of new array
+    public static int[] squaresOfSortedArray(int ar[]){
+        int[] res = new int[ar.length];
+        int l=0,r=ar.length-1,k=ar.length-1;
+        while(l<=r){
+            if((ar[l]*ar[l]) > (ar[r]* ar[r])){
+                res[k] = ar[l]*ar[l];
+                l++;
+            }else if((ar[l]*ar[l])<= (ar[r]* ar[r])){
+                res[k] = ar[r]* ar[r];
+                r--;
+            }
+            k--;
+        }
+        return res;
+    }
+}
